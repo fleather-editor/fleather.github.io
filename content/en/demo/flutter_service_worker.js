9752fd689f3e43fed643ea6e4c653a11ac541564
Alan Mantoux <alan.mantoux@gmail.com>
2023-06-15 09:43:27 +0200

    Update demo Modern heading selector

--- content/en/demo/flutter_service_worker.js
+++ content/en/demo/flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"version.json": "ff966ab969ba381b900e61629bfb9789",
 "index.html": "ecc976d5c4d73891d4e9d680f683ccbd",
 "/": "ecc976d5c4d73891d4e9d680f683ccbd",
-"main.dart.js": "18e414e7d56d738a187c01bfda661ea1",
+"main.dart.js": "1a95816064a55446ae0d80d7ffa60375",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
